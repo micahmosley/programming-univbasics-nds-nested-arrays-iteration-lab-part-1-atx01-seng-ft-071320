1820b0cbe33a7dda89f3093adce8b78fc36edd46
Micah Mosley <micah.mosley@utexas.edu>
2020-06-26 18:56:35 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,6 +6,13 @@ i=0
 while i < src.length do
   j=0 
   while j<src[i].length do 
-    if (src[i][j])%
+    if (src[i][j])%2==0 
+      puts src[i][j]
+    end 
+    j++
+  end
+  i++
+end 
+
   
 end
